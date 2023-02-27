4ba115b496b75eca44b91977156d8220ca22ea1a
Ayyan-durai <123533682+Ayyan-durai@users.noreply.github.com>
2023-02-27 18:06:57 +0530

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { AddComponent } from './component/add/add.component';
+import { MaterialModule } from './material/material.module';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {RouterLink, RouterLinkActive, RouterModule, RouterOutlet} from "@angular/router";
+import {RoutRoutingModule} from "./app-routing.module";
+import { BodyComponent } from './component/body/body.component';
+import { EditComponent } from './component/Edit-Delete/edit/edit.component';
+import { DeleteComponent } from './component/Edit-Delete/delete/delete.component';
+import {Ng2SearchPipeModule} from "ng2-search-filter";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AddComponent,
+    BodyComponent,
+    EditComponent,
+    DeleteComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        MaterialModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        RouterLink,
+        RouterLinkActive,
+        RouterOutlet,
+        RoutRoutingModule,
+        FormsModule,
+        Ng2SearchPipeModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
